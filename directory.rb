20209cd9b7ab8987b7112e9d14799bc4428edd9f
Rizwan Ali <rizcube@gmail.com>
2015-01-04 13:24:42 +0000

    Added an array and more information

--- directory.rb
+++ directory.rb
@@ -1,20 +1,24 @@
 
 # Let's put all students into an array
 students = [
-	"Sam Joseph",
-	"Kieran Goodacre",
-	"Christopher Ward",
-	"Rizwan Ali",
-	"The Joker",
-	"Freddy Kruger",
-	"The Alien",
-	"Michael Corleone",
-	"Nurse Ratched"
+	["Sam Joseph", :Feb],
+	["Kieran Goodacre", :Feb],
+	["Christopher Ward", :Feb],
+	["Rizwan Ali", :Feb],
+	["The Joker", :Feb],
+	["Freddy Kruger", :Feb],
+	["The Alien", :Feb],
+	["Michael Corleone", :Feb],
+	["Nurse Ratched", :Feb]
 ]
 
 	puts "The students of my cohort at the Makers Academy (Feb 2015)"
 	puts "----------------------------------------------------------"
+	
+def print(students)
 	students.each do |student|
-		puts student
+	  puts "#{student[0]} (#{student[1]} cohort)"
 	end
+end
+print(students)
 puts "Overall, we have #{students.length} great students"
