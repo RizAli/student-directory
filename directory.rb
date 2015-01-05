9d7fa162c6dcceb3ea6af48f7bbc312077e6ba36
Rizwan Ali <rizcube@gmail.com>
2015-01-05 17:49:18 +0000

    using hash made the code more readable

--- directory.rb
+++ directory.rb
@@ -1,15 +1,16 @@
 
 # Let's put all students into an array
+# lets put data in hashes
 students = [
-	["Sam Joseph", :Feb],
-	["Kieran Goodacre", :Feb],
-	["Christopher Ward", :Feb],
-	["Rizwan Ali", :Feb],
-	["The Joker", :Feb],
-	["Freddy Kruger", :Feb],
-	["The Alien", :Feb],
-	["Michael Corleone", :Feb],
-	["Nurse Ratched", :Feb]
+	{:name => "Sam Joseph", :cohort => :Feb},
+	{:name => "Kieran Goodacre", :cohort =>:Feb},
+	{:name => "Christopher Ward", :cohort =>:Feb},
+	{:name => "Rizwan Ali", :cohort =>:Feb},
+	{:name => "The Joker", :cohort => :Feb},
+	{:name =>"Freddy Kruger", :cohort => :Feb},
+	{:name => "The Alien", :cohort => :Feb},
+	{:name => "Michael Corleone", :cohort=>:Feb},
+	{:name => "Nurse Ratched", :cohort => :Feb}
 ]
 
 	puts "The students of my cohort at the Makers Academy (Feb 2015)"
@@ -17,7 +18,7 @@ students = [
 	
 def print(students)
 	students.each do |student|
-	  puts "#{student[0]} (#{student[1]} cohort)"
+	  puts "#{student[:name]} (#{student[:cohort]} cohort)"
 	end
 end
 print(students)
